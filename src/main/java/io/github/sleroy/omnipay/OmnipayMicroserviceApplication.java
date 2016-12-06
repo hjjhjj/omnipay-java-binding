fcb2a71640afa54732da3d58128328d9c412cb8f
sleroy <sylvain.leroy@tocea.com>
2016-12-06 11:23:27 +0100

    Initialization of the repository

---
+++ src/main/java/io/github/sleroy/omnipay/OmnipayMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package io.github.sleroy.omnipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnipayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnipayMicroserviceApplication.class, args);
+	}
+}
